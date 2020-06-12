44a139d9ce673c1b81f5b23a5d191465ba1340ad
zhengyu <yu_zheng@jsxzkingdee.com>
2020-06-12 17:38:33 +0800

    提交 镇江分支 state module模块

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -21,28 +21,4 @@ const app = {
   }
 }
 
-// 过滤菜单
-function filterMenu(routes) {
-  let arr = []
-  routes.forEach(route => {
-    const tmp = { ...route }
-    if (tmp.path !== '*' || !tmp.hidden) { // 过滤顶级菜单，即，404和已经隐藏的
-      if (tmp.children) {
-        tmp.children = filterMenu(tmp.children)
-      }
-      let obj = {}
-      obj.path = tmp.path // 这里的路由path默认都为不带/
-      obj.title = tmp.meta.title
-      if (tmp.meta.icon) {
-        obj.icon = tmp.meta.icon
-      }
-      if (tmp.children) {
-        obj.children = tmp.children
-      }
-      arr.push(obj)
-    }
-  })
-  return arr
-}
-
 export default app
