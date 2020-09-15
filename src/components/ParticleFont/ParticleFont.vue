da63ed7031490076cbe6062c0684bb83fac0681d
zhengyu <yu_zheng@jsxzkingdee.com>
2020-09-15 09:50:21 +0800

    update

--- src/components/ParticleFont/ParticleFont.vue
+++ src/components/ParticleFont/ParticleFont.vue
@@ -21,6 +21,45 @@
           '#8BC34A', '#CDDC39', '#FFEB3B', '#FFC107', '#FF9800',
           '#FF5722'
         ]
+      },
+      Particle(x, y) {
+        this.x = x;
+        this.y = y;
+        this.color = colors[Math.floor(Math.random() * colors.length)]; //'bleack'//
+        this.futurRadius = randomInt(1.1, 5.1);
+        this.radius = 1.1;
+        this.dying = false;
+        this.base = [x, y];
+
+        this.drawParticle = function () {
+          // 当前粒子变小到一定程度之后，每次将它的半径+0.1，使其慢慢变大
+          if (this.radius < this.futurRadius && this.dying === false) {
+            this.radius += durVal;
+          } else { //粒子已经到达最大状态
+            this.dying = true; //表示粒子还处于show状态
+          }
+
+          //每次-0.1
+          if (this.dying) {
+            this.radius -= durVal;
+          }
+          // 画粒子形状
+          context.save();
+          context.fillStyle = this.color;
+          context.beginPath();
+          context.fillRect(this.x, this.y, this.futurRadius, this.futurRadius)
+          context.closePath();
+          context.fill();
+          context.restore();
+
+          //将消失的粒子重置最初的状态
+          if (this.y < 0 || this.radius < 1) {
+            this.x = this.base[0];
+            this.y = this.base[1];
+            this.dying = false;
+            this.futurRadius = randomInt(1.1, 5.1);
+          }
+        }
       }
     }
   }
