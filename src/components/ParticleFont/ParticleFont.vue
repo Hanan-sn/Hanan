1de7faeca0915605fd07deef0e533334f345b08f
zhengyu <yu_zheng@jsxzkingdee.com>
2020-09-15 10:38:21 +0800

    update

--- src/components/ParticleFont/ParticleFont.vue
+++ src/components/ParticleFont/ParticleFont.vue
@@ -11,11 +11,23 @@
       this.renderParticleFont()
     },
     methods: {
-      renderParticleFont() {
+      renderParticleFont(text) {
         const canvas = this.$refs.particleFont
         const context = canvas.getContext('2d')
+        // 焦距
         focalllength = 250
-
+        // 文字内容转点阵数据
+        const dots = get
+      },
+      getImgData(w, h) {
+        const imgData = context.getImgData(0,0,w,h)
+        context.clearRect(0,0,w,h)
+        const dots = []
+        for (let x = 0; x < imgData.width; x+=6) {
+          for (let y = 0; y < imgData.height; y+=6) {
+            
+          }
+        }
       }
     }
   }
