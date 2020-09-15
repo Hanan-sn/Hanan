7fd2211d90a71c30b79bb4d201c6671b4df8de71
zhengyu <yu_zheng@jsxzkingdee.com>
2020-09-15 09:49:21 +0800

    update

--- src/components/ParticleFont/ParticleFont.vue
+++ src/components/ParticleFont/ParticleFont.vue
@@ -16,6 +16,11 @@
         context.textAlign = 'center'
         context.font = "18px arial";
         context.fillText('hello', this.x, this.y);
+        const colors = ['#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5',
+          '#2196f3', '#03a9f4', '#00bcd4', '#009688', '#4CAF50',
+          '#8BC34A', '#CDDC39', '#FFEB3B', '#FFC107', '#FF9800',
+          '#FF5722'
+        ]
       }
     }
   }
