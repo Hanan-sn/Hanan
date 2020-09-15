cbc3170fe47f68332999dab6eb57809b19c7f778
zhengyu <yu_zheng@jsxzkingdee.com>
2020-09-15 10:42:21 +0800

    update

--- src/components/ParticleFont/ParticleFont.vue
+++ src/components/ParticleFont/ParticleFont.vue
@@ -20,10 +20,14 @@
         const dots = get
       },
       getImgData(w, h) {
-        const Dot = 
+        const Dot =
         const imgData = context.getImageData(0,0,w,h)
         context.clearRect(0,0,w,h)
-        const dots = []
+        class Dot{
+          constructor(centerX, centerY, centerZ, radius){
+            
+          }
+        }
         for (let x = 0; x < imgData.width; x+=6) {
           for (let y = 0; y < imgData.height; y+=6) {
             let i = (y*imgData.width + x) * 4
