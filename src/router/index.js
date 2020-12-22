00052372873848155d04ef3dce55038e52f8b408
zhengyu <yu_zheng@jsxzkingdee.com>
2020-12-22 13:55:37 +0800

    提交 初始化 1.0.5

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import BinUI from 'bin-ui'
+// import BinUI from 'bin-ui'
 
 const Home = () => import('../pages/Home/Home')
 const Overview = () => import('../pages/OverviewPage/Overview')
@@ -8,7 +8,7 @@ const Summary = () => import('../pages/SummaryPage/Summary')
 const Public = () => import('../pages/PublicPage/Public')
 const Union = () => import('../pages/UnionPage/Union')
 
-Vue.use(BinUI)
+// Vue.use(BinUI)
 
 Vue.use(VueRouter)
 
@@ -33,10 +33,6 @@ const router = new VueRouter({
     {
       path: '/union',
       component: Union
-    },
-    {
-      path: '/map',
-      component: Map
     },
     {
       path: '/',
